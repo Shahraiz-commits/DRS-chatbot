6233ace4c20709fd8540513dd4b27b02b59cddce
Shahraiz-commits <azizshahraiz12@gmail.com>
2025-01-30 17:00:35 -0500

    Added level0/homepage questions

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -42,20 +42,35 @@ class ActionLibraryOpen(Action):
         currentDay = datetime.datetime.now().strftime("%A")
         currentTime = datetime.datetime.now().time()
         
+        # TODO: Make the hours dynamic. Account for holidays and emergency closures.
         # Hours for students/faculty not visitors
         library_hours = {
-            "Sunday": (datetime.time(10, 0), datetime.time(2, 0)),  # 10:00 AM - 2:00 AM
             "Monday": (datetime.time(7, 30), datetime.time(2, 0)),  # 7:30 AM - 2:00 AM
             "Tuesday": (datetime.time(7, 30), datetime.time(2, 0)), # 7:30 AM - 2:00 AM
             "Wednesday": (datetime.time(7, 30), datetime.time(2, 0)), # 7:30 AM - 2:00 AM
             "Thursday": (datetime.time(7, 30), datetime.time(2, 0)), # 7:30 AM - 2:00 AM
             "Friday": (datetime.time(7, 30), datetime.time(19, 0)),  # 7:30 AM - 7:00 PM
-            "Saturday": (datetime.time(10, 0), datetime.time(19, 0))  # 10:00 AM - 7:00 PM
+            "Saturday": (datetime.time(10, 0), datetime.time(19, 0)),  # 10:00 AM - 7:00 PM
+            "Sunday": (datetime.time(10, 0), datetime.time(2, 0)),  # 10:00 AM - 2:00 AM
         }
 
-        # Check if the library is open based on the day
-        open_time, close_time = library_hours.get(currentDay, (None, None))
-        is_open = open_time <= currentTime <= close_time
-        response = f"The library is open until {close_time}." if is_open else f"The library is currently closed. It will open at {open_time}"
+        open_time, close_time = library_hours.get(currentDay)
+        
+        # Get times for +1/-1 days for checking past-midnight hours
+        days = list(library_hours.keys())
+        prev_day = days[(days.index(currentDay) - 1) % len(days)]
+        next_day = days[(days.index(currentDay) + 1) % len(days)]
+        prev_open, prev_close = library_hours[prev_day]
+        next_open, next_close = library_hours[next_day]
+
+
+        if open_time < close_time:
+            # Normal case: opens and closes within the same day
+            is_open = open_time <= currentTime < close_time
+        else:
+            # Special case: closes after midnight
+            is_open = (currentTime >= open_time) or (currentTime < prev_close)
+        
+        response = f"The library is currently open and will close at {close_time.strftime('%I:%M %p')}." if is_open else f"The library is currently closed. It will open at {next_open.strftime('%I:%M %p')} on {next_day} and stay open until {next_close.strftime('%I:%M %p')}."
         dispatcher.utter_message(response)
         return []
